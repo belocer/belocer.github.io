283d2ce6f82652fd642f8f671958efd441f63836
Denis Belocerkovec <belocerkovecden@gmail.com>
2017-05-09 16:35:03 +0300

    Update index.js

--- lesson_20/js/index.js
+++ lesson_20/js/index.js
@@ -1,6 +1,6 @@
 $('document').ready(function() {
-	var options = { videoId: 'aRGz0fNasiA', start: 3 };
-	$('.header').tubular(options);
+	var options = { videoId: 'aRGz0fNasiA', start: 0 };
+	$('#header').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
 });
