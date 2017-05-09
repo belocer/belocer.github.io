08f44f7dcc6fb8f0e5bc4670256b557e9b46222e
Denis Belocerkovec <belocerkovecden@gmail.com>
2017-05-09 12:42:28 +0300

    less 20

---
+++ lesson_20/js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'aRGz0fNasiA', start: 3 };
+	$('.header').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
