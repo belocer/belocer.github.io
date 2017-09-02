88f5c4bb736e97e7cb520b4b8986885a3778eefc
Denis Belocerkovec <belocerkovecden@gmail.com>
2017-09-02 11:40:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     particlesJS.load('particles-js', 'json/particles.json', function() {
-        console.log('callback - particles.js config loaded');
+        //console.log('callback - particles.js config loaded');
     });
 });
